7b7e14913378ef34577dd32edb48b1195c882921
Sharad Shinde <shindesharad71@gmail.com>
2019-03-18 19:57:01 +0530

    app and server seperated

---
+++ server/src/configs/app.ts
@@ -0,0 +1,58 @@
+import bodyParser from 'body-parser';
+import chalk from "chalk";
+import cors from 'cors';
+import dotenv from "dotenv";
+import express from "express";
+import jwt from 'express-jwt';
+import mongoose from "mongoose";
+
+import { Routes } from '../routes';
+import { JWT_CONFIG, requestValidator } from './jwt';
+
+// Init dotenv config
+dotenv.config();
+
+const app = express();
+
+// Connect to MongoDB.
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useNewUrlParser", true);
+mongoose.connect(`${process.env.MONGO_URL}${process.env.MONGO_DB_NAME}`);
+mongoose.connection.on("error", (err) => {
+    console.error(err);
+    console.log("%s MongoDB connection error. Please make sure MongoDB is running.", chalk.red("✗"));
+    process.exit();
+});
+
+// Middleware Initialization
+app.use(cors());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
+app.use('/public', express.static('uploads'));
+app.use(jwt({
+    secret: JWT_CONFIG.JWT_SECRET,
+}).unless({ path: JWT_CONFIG.noAuthUrls }));
+
+// Request, Response Entry Point
+app.use((req, res, next) => {
+    console.log(chalk.yellow(`${req.method} - ${req.url}`));
+    if (requestValidator(req)) {
+        next();
+    } else {
+        res.status(400).json({
+            message: 'invalid auth token, or token is not supplied'
+        });
+    }
+});
+
+// Routes
+Routes.forEach((route) => app.use(route.path, route.router));
+
+app.use('*', (req, res, next) => {
+    const message = `Cannot ${req.method} ${req.url}`;
+    res.status(404).json({ error: message });
+    next();
+});
+
+export { app };
