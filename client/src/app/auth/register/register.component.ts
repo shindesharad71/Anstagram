8d7c7685910c89d96b9bb5e49ad05b0293e99df4
Sharad Shinde <shindesharad71@gmail.com>
2019-03-20 22:06:51 +0530

    Register form controls added

--- client/src/app/auth/register/register.component.ts
+++ client/src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
 
 @Component({
   selector: 'ia-register',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  isEmailInvalid = false;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.registerForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+    });
+  }
+
+  register() {
+    this.authService.login(this.registerForm.value).subscribe(res => {
+      console.log(res);
+    }, err => {
+      console.log(err);
+    });
+  }
+
+  onInputBlur() {
+    this.isEmailInvalid = this.registerForm.controls.email.value && !this.registerForm.controls.email.valid;
   }
 
 }
