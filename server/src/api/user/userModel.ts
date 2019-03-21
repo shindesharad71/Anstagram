5e805ea949858530635021cb0f96535c86c179b3
Sharad Shinde <shindesharad71@gmail.com>
2019-03-21 11:25:16 +0530

    user schema modified

--- server/src/api/user/userModel.ts
+++ server/src/api/user/userModel.ts
@@ -7,19 +7,23 @@ type UserType = mongoose.Document & {
     id: string,
     firstName: string,
     lastName: string,
+    gender: string,
     email: string,
     password: string,
+    dateOfBirth: string
 };
 
 const userSchema = new Schema({
     id: { type: ObjectId, index: true },
     firstName: { type: String, trim: true, required: 'required firstName' },
     lastName: { type: String, trim: true, required: 'required lastName' },
+    gender: { type: String, required: 'required gender' },
     email: {
         type: String, unique: true, required: 'required email', index: true, lowercase: true
     },
     dateOfBirth: { type: Date, required: 'required dateOfBirth' },
     password: { type: String, required: 'required password' },
+    resetOtp: { type: Number },
 }, {
         timestamps: true
     });
