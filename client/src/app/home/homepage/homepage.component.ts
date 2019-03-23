adb360c31fa8c66caecf9711372fbcd2d8434ad4
Sharad Shinde <shindesharad71@gmail.com>
2019-03-24 01:02:16 +0530

    at bottom method added

--- client/src/app/home/homepage/homepage.component.ts
+++ client/src/app/home/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { AuthService } from 'src/app/core/services/auth/auth.service';
 import { Router } from '@angular/router';
 import { Title } from '@angular/platform-browser';
@@ -8,6 +8,7 @@ import { Title } from '@angular/platform-browser';
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
+
 export class HomepageComponent implements OnInit {
 
   constructor(private authService: AuthService, private router: Router, private titleService: Title) {
@@ -15,6 +16,13 @@ export class HomepageComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      console.log('at bottom');
+    }
   }
 
 }
