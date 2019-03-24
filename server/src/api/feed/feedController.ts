2dd560296b90b4bf82565bb556fd605e05af85d2
Sharad Shinde <shindesharad71@gmail.com>
2019-03-24 16:15:14 +0530

    feed create and feed get api added

--- server/src/api/feed/feedController.ts
+++ server/src/api/feed/feedController.ts
@@ -2,7 +2,8 @@ import { Feed, FeedType } from './feedModel';
 
 const getUserFeed = async (req: any, res: any) => {
     try {
-        res.json({ message: req.user });
+        const feed = await Feed.find({});
+        res.json(feed);
     } catch (error) {
         res.status(400).json({ error: error.name, message: error.message });
         throw error;
@@ -11,7 +12,13 @@ const getUserFeed = async (req: any, res: any) => {
 
 const addUserFeed = async (req: any, res: any) => {
     try {
-        res.json({ message: req.user });
+        const feed = new Feed({
+            userId: req.user,
+            media: req.body.media,
+            description: req.body.description
+        });
+        await feed.save();
+        res.status(201).json({ message: `feed created successfully` });
     } catch (error) {
         res.status(400).json({ error: error.name, message: error.message });
         throw error;
