34fcbbc22d3ae22fb2fd01687da401218303a2e1
Sharad Shinde <shindesharad71@gmail.com>
2019-03-24 13:48:29 +0530

    mongodb config updated

--- server/src/configs/app.ts
+++ server/src/configs/app.ts
@@ -18,7 +18,12 @@ const app = express();
 mongoose.set("useFindAndModify", false);
 mongoose.set("useCreateIndex", true);
 mongoose.set("useNewUrlParser", true);
-mongoose.connect(`${process.env.MONGO_URL}${process.env.MONGO_DB_NAME}`);
+
+// tslint:disable-next-line: max-line-length
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URL}${process.env.MONGO_DB_NAME}`, {
+    useNewUrlParser: true
+});
+
 mongoose.connection.on("error", (err) => {
     console.error(err);
     console.log("%s MongoDB connection error. Please make sure MongoDB is running.", chalk.red("✗"));
