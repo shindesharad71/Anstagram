b36dc344a4e1e3e7088ad554753bc45c00b4398b
Sharad Shinde <shindesharad71@gmail.com>
2019-03-26 23:27:42 +0530

    server start code changed

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,7 @@
 import chalk from 'chalk';
-import dotenv from "dotenv";
 import { app } from './configs/app';
 
-dotenv.config();
-
 // Start Server
-app.listen(process.env.PORT, () => {
-    console.log(chalk.green(`✗ Server started at http://localhost:${process.env.PORT}`));
+app.listen(8080, '0.0.0.0', () => {
+    console.log(chalk.green(`✗ Server started at http://0.0.0.0:8080`));
 });
