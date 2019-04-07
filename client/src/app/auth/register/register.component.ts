b6c168640afe3332a3c6d7b5d70c791e0b705664
Sharad Shinde <shindesharad71@gmail.com>
2019-04-07 13:27:54 +0530

    debounce added

--- client/src/app/auth/register/register.component.ts
+++ client/src/app/auth/register/register.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/core/services/auth/auth.service';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { Subscription, Subject } from 'rxjs';
 
 @Component({
   selector: 'ia-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
 
   registerForm: FormGroup;
   isEmailInvalid = false;
@@ -17,6 +19,8 @@ export class RegisterComponent implements OnInit {
   isRegistrationComplete = false;
   errorMessage: string;
   notificationType = 'is-danger';
+  usernameSubscription: Subscription;
+  usernameSubject = new Subject<string>();
 
   constructor(private authService: AuthService, private titleService: Title) {
     this.titleService.setTitle('Register');
@@ -35,6 +39,18 @@ export class RegisterComponent implements OnInit {
       gender: new FormControl('', [Validators.required]),
       termsAndConditions: new FormControl('', [Validators.required])
     });
+
+    this.usernameSubscription = this.usernameSubject.pipe(debounceTime(1000), distinctUntilChanged()).subscribe(usernameInput => {
+      if (usernameInput && usernameInput.length > 3) {
+        this.checkAvaibilityOfUsername(usernameInput);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.usernameSubscription) {
+      this.usernameSubscription.unsubscribe();
+    }
   }
 
   register() {
@@ -55,9 +71,17 @@ export class RegisterComponent implements OnInit {
     this.isPasswordsMatch = this.registerForm.controls.password.value === this.registerForm.controls.confirmPassword.value;
   }
 
-  checkUsername() {
-// tslint:disable-next-line: max-line-length
+  checkUsername(usernameInput) {
+    // tslint:disable-next-line: max-line-length
     this.isUsernameInvalid = this.registerForm.controls.username.touched && this.registerForm.controls.username.dirty && !this.registerForm.controls.username.valid;
+
+    if (!this.isUsernameInvalid) {
+      this.usernameSubject.next(this.registerForm.controls.username.value);
+    }
+  }
+
+  checkAvaibilityOfUsername(usernameInput) {
+    console.log(usernameInput);
   }
 
 }
