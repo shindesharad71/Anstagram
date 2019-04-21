c8a3a639daa79d8682b3bb1093c01db6df7b70b0
Sharad Shinde <shindesharad71@gmail.com>
2019-04-21 14:03:45 +0530

    profile updated

--- client/src/app/profile/profile.module.ts
+++ client/src/app/profile/profile.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+library.add(fas);
+
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
@@ -9,7 +14,8 @@ import { ProfilePageComponent } from './profile-page/profile-page.component';
   declarations: [ProfileComponent, ProfilePageComponent],
   imports: [
     CommonModule,
-    ProfileRoutingModule
+    ProfileRoutingModule,
+    FontAwesomeModule
   ]
 })
 export class ProfileModule { }
