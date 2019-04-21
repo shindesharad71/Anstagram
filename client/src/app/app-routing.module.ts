908c423824547e0ddb95836a34d7c6b78444fc46
Sharad Shinde <shindesharad71@gmail.com>
2019-04-21 11:42:13 +0530

    404 route added

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { AuthGuardService } from './core/guards/auth-guard.service';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { CreatePostComponent } from './home/create-post/create-post.component';
 import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
 import { HomepageComponent } from './home/homepage/homepage.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent, canActivate: [AuthGuardService], pathMatch: 'full' },
@@ -13,6 +15,7 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'create', component: CreatePostComponent, canActivate: [AuthGuardService] },
+  { path: '404', component: PageNotFoundComponent },
   { path: '**', loadChildren: '../app/profile/profile.module#ProfileModule', canActivate: [AuthGuardService]}
 ];
 
