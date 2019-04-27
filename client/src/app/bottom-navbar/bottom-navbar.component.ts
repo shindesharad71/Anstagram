daa0cd724f53eea4d72c466b4aa79172960ade7c
Sharad Shinde <shindesharad71@gmail.com>
2019-04-27 23:04:39 +0530

    bottom bar added

---
+++ client/src/app/bottom-navbar/bottom-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ia-bottom-navbar',
+  templateUrl: './bottom-navbar.component.html',
+  styleUrls: ['./bottom-navbar.component.scss']
+})
+export class BottomNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
