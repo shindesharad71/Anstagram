538f5910df678a4d0d7a9fd695984f69230c1385
Sharad Shinde <shindesharad71@gmail.com>
2019-04-27 22:11:42 +0530

    new modules created

---
+++ client/src/app/explore/explore.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ExploreRoutingModule } from './explore-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ExploreRoutingModule
+  ]
+})
+export class ExploreModule { }
---
+++ client/src/app/notification/notification.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NotificationRoutingModule } from './notification-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NotificationRoutingModule
+  ]
+})
+export class NotificationModule { }
