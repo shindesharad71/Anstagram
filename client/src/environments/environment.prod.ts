fcf8a141fe87cf20d91eaef25e0f83f3950471ad
Sharad Shinde <shindesharad71@gmail.com>
2019-04-28 16:19:47 +0530

    new build again, server issues

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // BASE_URL: 'https://anstagram-api.sharadshinde.in/'
-  BASE_URL: 'http://35.237.71.90/'
+  BASE_URL: 'https://anstagram-api.sharadshinde.in/'
 };
