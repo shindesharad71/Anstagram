5b86c2968513bad72e13b2ba57a535311b167a67
Sharad Shinde <sharad.shinde@coditas.com>
2020-08-14 19:54:21 +0530

    Updated: Coockies to Sessionstorage

--- apps/frontend/src/app/auth/login/login.component.ts
+++ apps/frontend/src/app/auth/login/login.component.ts
@@ -6,67 +6,79 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { HttpService } from '../../core/services/http/http.service';
 
 @Component({
-  selector: 'ia-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+	selector: 'ia-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
-  errorMessage: string;
-  notificationType = 'is-danger';
+	loginForm: FormGroup;
+	errorMessage: string;
+	notificationType = 'is-danger';
 
-  // tslint:disable-next-line: max-line-length
-  constructor(private authService: AuthService, private titleService: Title, private router: Router, private route: ActivatedRoute, private httpService: HttpService) {
-    this.checkForEmailVerification();
-    this.titleService.setTitle('Anstagram - Login');
-    if (this.authService.checkToken()) {
-      this.router.navigateByUrl('/');
-    }
-  }
+	constructor(
+		private authService: AuthService,
+		private titleService: Title,
+		private router: Router,
+		private route: ActivatedRoute,
+		private httpService: HttpService
+	) {
+		this.checkForEmailVerification();
+		this.titleService.setTitle('Anstagram - Login');
+		if (this.authService.checkToken()) {
+			this.router.navigateByUrl('/');
+		}
+	}
 
-  ngOnInit() {
-    this.loginForm = new FormGroup({
-      loginInput: new FormControl('', [Validators.required]),
-      password: new FormControl('', [Validators.required])
-    });
-  }
+	ngOnInit() {
+		this.loginForm = new FormGroup({
+			loginInput: new FormControl('', [Validators.required]),
+			password: new FormControl('', [Validators.required]),
+		});
+	}
 
-  login() {
-    this.errorMessage = null;
-    if (this.loginForm.valid) {
-      this.httpService.post('users/login', this.loginForm.value).subscribe((res: any) => {
-        this.authService.setToken(res.token);
-        this.authService.setUsername(res.username);
-        if (this.authService.checkToken()) {
-          this.router.navigateByUrl('/');
-        } else {
-          this.router.navigateByUrl('/login');
-        }
-      }, err => {
-        this.errorMessage = err.error.message;
-        console.log(err);
-      });
-    }
-  }
+	login() {
+		this.errorMessage = null;
+		if (this.loginForm.valid) {
+			this.httpService
+				.post('users/login', this.loginForm.value)
+				.subscribe(
+					(res: any) => {
+						this.authService.setToken(res.token);
+						this.authService.setUsername(res.username);
+						if (this.authService.checkToken()) { 
+							this.router.navigateByUrl('/');
+						} else {
+							this.router.navigateByUrl('/login');
+						}
+					},
+					(err) => {
+						this.errorMessage = err.error.message;
+						console.log(err);
+					}
+				);
+		}
+	}
 
-  checkForEmailVerification() {
-    this.errorMessage = null;
-    let urlParams = null;
-    this.route.queryParams.subscribe(params => {
-      urlParams = params;
-    });
+	checkForEmailVerification() {
+		this.errorMessage = null;
+		let urlParams = null;
+		this.route.queryParams.subscribe((params) => {
+			urlParams = params;
+		});
 
-    if (urlParams && urlParams['query']) {
-      const query = { query: urlParams['query'] };
-      this.httpService.post('users/verify', query).subscribe((res: any) => {
-        this.notificationType = 'is-success';
-        this.errorMessage = res.message;
-        console.log(res);
-      }, err => {
-        this.errorMessage = err.error.message;
-        console.log(err);
-      });
-    }
-  }
-
+		if (urlParams && urlParams['query']) {
+			const query = { query: urlParams['query'] };
+			this.httpService.post('users/verify', query).subscribe(
+				(res: any) => {
+					this.notificationType = 'is-success';
+					this.errorMessage = res.message;
+					console.log(res);
+				},
+				(err) => {
+					this.errorMessage = err.error.message;
+					console.log(err);
+				}
+			);
+		}
+	}
 }
--- apps/frontend/src/app/core/services/auth/auth.service.ts
+++ apps/frontend/src/app/core/services/auth/auth.service.ts
@@ -1,46 +1,43 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { CookieService } from 'ngx-cookie-service';
 import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root',
 })
 export class AuthService {
+	private isLoggedInSource = new Subject();
+	isLoggedIn = this.isLoggedInSource.asObservable();
 
-  private isLoggedInSource = new Subject();
-  isLoggedIn = this.isLoggedInSource.asObservable();
+	constructor() {}
 
-  constructor(private http: HttpClient, private cookieService: CookieService) { }
+	checkToken(): boolean {
+		this.isLoggedInSource.next('_auth' in sessionStorage);
+		return '_auth' in sessionStorage;
+	}
 
-  checkToken(): boolean {
-    this.isLoggedInSource.next(this.cookieService.check('_auth'));
-    return this.cookieService.check('_auth');
-  }
+	getToken() {
+		return sessionStorage.getItem('_auth');
+	}
 
-  getToken() {
-    return this.cookieService.get('_auth');
-  }
+	setToken(token: string) {
+		sessionStorage.setItem('_auth', token);
+		return true;
+	}
 
-  setToken(token: string) {
-    this.cookieService.set('_auth', token);
-    return true;
-  }
+	removeToken() {
+		sessionStorage.clear();
+		if (!this.checkToken()) {
+			return true;
+		}
+		return false;
+	}
 
-  removeToken() {
-    this.cookieService.deleteAll();
-    if (!this.checkToken()) {
-      return true;
-    }
-    return false;
-  }
+	setUsername(username) {
+		sessionStorage.setItem('username', username);
+		return true;
+	}
 
-  setUsername(username) {
-    this.cookieService.set('username', username);
-    return true;
-  }
-
-  getUsername() {
-    return this.cookieService.get('username');
-  }
+	getUsername() {
+		return sessionStorage.getItem('username');
+	}
 }
