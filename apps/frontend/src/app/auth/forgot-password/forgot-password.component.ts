65524c462fb5651f0d5656fda60844fceae451c6
Sharad Shinde <sharad.shinde@coditas.com>
2020-11-13 12:07:07 +0530

    Updated: Icons Version and New Way to Add Icons

--- apps/frontend/src/app/auth/forgot-password/forgot-password.component.ts
+++ apps/frontend/src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,43 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+	faEnvelope,
+	faExclamationTriangle
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'ia-forgot-password',
-  templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.scss']
+	selector: 'ia-forgot-password',
+	templateUrl: './forgot-password.component.html',
+	styleUrls: ['./forgot-password.component.scss']
 })
 export class ForgotPasswordComponent implements OnInit {
-  forgotPasswordForm: FormGroup;
-  isEmailInvalid = false;
-  isError = false;
-  errorMessage = '';
+	// Icons
+	exclamationTriangle = faExclamationTriangle;
+	envelope = faEnvelope;
 
-  constructor(private titleService: Title) {
-    this.titleService.setTitle('Anstagram - Forgot Password');
-  }
+	forgotPasswordForm: FormGroup;
+	isEmailInvalid = false;
+	isError = false;
+	errorMessage = '';
 
-  ngOnInit() {
-    this.forgotPasswordForm = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email])
-    });
-  }
+	constructor(private titleService: Title) {
+		this.titleService.setTitle('Anstagram - Forgot Password');
+	}
 
-  forgotPassword() {
-    console.log('forgot password');
-  }
+	ngOnInit() {
+		this.forgotPasswordForm = new FormGroup({
+			email: new FormControl('', [Validators.required, Validators.email])
+		});
+	}
 
-  onInputBlur() {
-    this.isEmailInvalid = this.forgotPasswordForm.controls.email.value && !this.forgotPasswordForm.controls.email.valid;
-  }
+	forgotPassword() {
+		console.log('forgot password');
+	}
 
-  removeErrorMessage() {
-    if (document.readyState === 'complete') {
-      const allNotifications = document.querySelectorAll('.notification');
-      allNotifications.forEach((notificationToDelete: any) => {
-        notificationToDelete.remove();
-      });
-    }
-  }
+	onInputBlur() {
+		this.isEmailInvalid =
+			this.forgotPasswordForm.controls.email.value &&
+			!this.forgotPasswordForm.controls.email.valid;
+	}
 
+	removeErrorMessage() {
+		if (document.readyState === 'complete') {
+			const allNotifications = document.querySelectorAll('.notification');
+			allNotifications.forEach((notificationToDelete: any) => {
+				notificationToDelete.remove();
+			});
+		}
+	}
 }
--- apps/frontend/src/app/auth/register/register.component.ts
+++ apps/frontend/src/app/auth/register/register.component.ts
@@ -1,103 +1,151 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { AuthService } from '../../core/services/auth/auth.service';
+import {
+	faCalendar,
+	faCheck,
+	faEnvelope,
+	faExclamationTriangle,
+	faIdCard,
+	faKey,
+	faUser
+} from '@fortawesome/free-solid-svg-icons';
+import { Subject, Subscription } from 'rxjs';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
-import { Subscription, Subject } from 'rxjs';
+import { AuthService } from '../../core/services/auth/auth.service';
 import { HttpService } from '../../core/services/http/http.service';
 
 @Component({
-  selector: 'ia-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+	selector: 'ia-register',
+	templateUrl: './register.component.html',
+	styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit, OnDestroy {
+	// Icons
+	user = faUser;
+	exclamationTriangle = faExclamationTriangle;
+	idCard = faIdCard;
+	check = faCheck;
+	key = faKey;
+	calendar = faCalendar;
+	envelope = faEnvelope;
 
-  registerForm: FormGroup;
-  isEmailInvalid = false;
-  isUsernameInvalid = false;
-  isPasswordsMatch = true;
-  isRegistrationComplete = false;
-  errorMessage: string;
-  notificationType = 'is-danger';
-  usernameSubscription: Subscription;
-  usernameSubject = new Subject<string>();
-  isUsernameAvailable = false;
+	registerForm: FormGroup;
+	isEmailInvalid = false;
+	isUsernameInvalid = false;
+	isPasswordsMatch = true;
+	isRegistrationComplete = false;
+	errorMessage: string;
+	notificationType = 'is-danger';
+	usernameSubscription: Subscription;
+	usernameSubject = new Subject<string>();
+	isUsernameAvailable = false;
 
-  constructor(private authService: AuthService, private titleService: Title, private httpService: HttpService) {
-    this.titleService.setTitle('Register');
-  }
+	constructor(
+		private authService: AuthService,
+		private titleService: Title,
+		private httpService: HttpService
+	) {
+		this.titleService.setTitle('Register');
+	}
 
-  ngOnInit() {
-    this.registerForm = new FormGroup({
-      firstName: new FormControl('', [Validators.required]),
-      lastName: new FormControl('', [Validators.required]),
-      // tslint:disable-next-line: max-line-length
-      email: new FormControl('', [Validators.required, Validators.email, Validators.pattern(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i)]),
-      username: new FormControl('', [Validators.min(4), Validators.pattern(/^[a-zA-Z0-9-_]+$/)]),
-      password: new FormControl('', [Validators.required]),
-      confirmPassword: new FormControl('', [Validators.required]),
-      dateOfBirth: new FormControl('', [Validators.required]),
-      gender: new FormControl('', [Validators.required]),
-      termsAndConditions: new FormControl('', [Validators.required])
-    });
+	ngOnInit() {
+		this.registerForm = new FormGroup({
+			firstName: new FormControl('', [Validators.required]),
+			lastName: new FormControl('', [Validators.required]),
+			// tslint:disable-next-line: max-line-length
+			email: new FormControl('', [
+				Validators.required,
+				Validators.email,
+				Validators.pattern(
+					/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+				)
+			]),
+			username: new FormControl('', [
+				Validators.min(4),
+				Validators.pattern(/^[a-zA-Z0-9-_]+$/)
+			]),
+			password: new FormControl('', [Validators.required]),
+			confirmPassword: new FormControl('', [Validators.required]),
+			dateOfBirth: new FormControl('', [Validators.required]),
+			gender: new FormControl('', [Validators.required]),
+			termsAndConditions: new FormControl('', [Validators.required])
+		});
 
-    this.usernameSubscription = this.usernameSubject.pipe(debounceTime(1000), distinctUntilChanged()).subscribe(usernameInput => {
-      if (usernameInput && usernameInput.length > 3) {
-        this.checkAvailabilityOfUsername(usernameInput);
-      }
-    });
-  }
+		this.usernameSubscription = this.usernameSubject
+			.pipe(debounceTime(1000), distinctUntilChanged())
+			.subscribe(usernameInput => {
+				if (usernameInput && usernameInput.length > 3) {
+					this.checkAvailabilityOfUsername(usernameInput);
+				}
+			});
+	}
 
-  ngOnDestroy() {
-    if (this.usernameSubscription) {
-      this.usernameSubscription.unsubscribe();
-    }
-  }
+	ngOnDestroy() {
+		if (this.usernameSubscription) {
+			this.usernameSubscription.unsubscribe();
+		}
+	}
 
-  register() {
-    this.errorMessage = null;
-    if (this.isUsernameAvailable) {
-      this.httpService.post('users/register', this.registerForm.value).subscribe(res => {
-        this.isRegistrationComplete = true;
-      }, err => {
-        this.errorMessage = err.error.message;
-        console.log(err);
-      });
-    } else {
-      this.errorMessage = 'Please check your username and try again';
-    }
-  }
+	register() {
+		this.errorMessage = null;
+		if (this.isUsernameAvailable) {
+			this.httpService
+				.post('users/register', this.registerForm.value)
+				.subscribe(
+					res => {
+						this.isRegistrationComplete = true;
+					},
+					err => {
+						this.errorMessage = err.error.message;
+						console.log(err);
+					}
+				);
+		} else {
+			this.errorMessage = 'Please check your username and try again';
+		}
+	}
 
-  checkEmail() {
-    this.isEmailInvalid = this.registerForm.controls.email.value && !this.registerForm.controls.email.valid;
-  }
+	checkEmail() {
+		this.isEmailInvalid =
+			this.registerForm.controls.email.value &&
+			!this.registerForm.controls.email.valid;
+	}
 
-  validatePassword() {
-    this.isPasswordsMatch = this.registerForm.controls.password.value === this.registerForm.controls.confirmPassword.value;
-  }
+	validatePassword() {
+		this.isPasswordsMatch =
+			this.registerForm.controls.password.value ===
+			this.registerForm.controls.confirmPassword.value;
+	}
 
-  checkUsername() {
-    this.isUsernameAvailable = false;
-    // tslint:disable-next-line: max-line-length
-    this.isUsernameInvalid = this.registerForm.controls.username.touched && this.registerForm.controls.username.dirty && !this.registerForm.controls.username.valid;
+	checkUsername() {
+		this.isUsernameAvailable = false;
+		// tslint:disable-next-line: max-line-length
+		this.isUsernameInvalid =
+			this.registerForm.controls.username.touched &&
+			this.registerForm.controls.username.dirty &&
+			!this.registerForm.controls.username.valid;
 
-    if (!this.isUsernameInvalid) {
-      this.usernameSubject.next(this.registerForm.controls.username.value);
-    }
-  }
+		if (!this.isUsernameInvalid) {
+			this.usernameSubject.next(
+				this.registerForm.controls.username.value
+			);
+		}
+	}
 
-  checkAvailabilityOfUsername(usernameInput) {
-    this.httpService.get(`users/check/${usernameInput}`).subscribe(res => {
-      if (!res) {
-        this.isUsernameAvailable = true;
-      } else {
-        this.isUsernameAvailable = false;
-      }
-    }, err => {
-      this.isUsernameAvailable = false;
-      console.log(err);
-    });
-  }
-
+	checkAvailabilityOfUsername(usernameInput) {
+		this.httpService.get(`users/check/${usernameInput}`).subscribe(
+			res => {
+				if (!res) {
+					this.isUsernameAvailable = true;
+				} else {
+					this.isUsernameAvailable = false;
+				}
+			},
+			err => {
+				this.isUsernameAvailable = false;
+				console.log(err);
+			}
+		);
+	}
 }
